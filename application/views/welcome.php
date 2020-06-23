73a8d4ef6ff1a98221e5c6739f5cb60e9c82b980
Timur Issaev <timka_issaev@mail.ru>
2020-06-23 17:27:13 +0300

    form was improved

--- application/views/welcome.php
+++ application/views/welcome.php
@@ -2,8 +2,8 @@
     <div class="wrap_2">
         <div>
             <div class="title">Создание записи</div>
-            <div class="input_wrap"><input type="text" class="text_field" placeholder="Заголовок записи"></div>
-            <div class="input_wrap"><TEXTAREA class="text_field" placeholder="Заголовок записи"></TEXTAREA></div>
+            <div class="input_wrap"><input type="text" class="text_field" placeholder="Заголовок записи" autofocus></div>
+            <div class="input_wrap"><TEXTAREA class="text_field textarea" placeholder="Описание записи"></TEXTAREA></div>
             <div class="input_wrap"><input type="submit" class="submit" value="Сохранить"></div>
         </div>
     </div>
@@ -11,12 +11,14 @@
 
 <style>
 
-.wrap_2{max-width:310px;margin:0 auto;padding:74px 0 4px;}
+.wrap_2{max-width:270px;margin:0 auto;padding:74px 0 4px;}
 .text_field{width:100%;}
-.input_wrap{margin:3px 0 7px;}
+.input_wrap{margin:5px 0 17px;}
 
-.title{font-size:17px;margin-bottom:14px;}
+.title{font-size:17px;margin-bottom:14px;text-align:center;text-transform:uppercase;font-weight:bold}
 
-.text_field{}
+.text_field{border:1px solid #CCC;padding:7px 11px;border-radius:5px;}
+.textarea{height:54px;}
+.submit{border:1px solid #CCC;padding:7px 11px;border-radius:5px;background:#FBFBFB;color:#000;}
 
 </style>
